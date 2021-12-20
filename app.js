ca806ccd1807b3bdfeb5f7a7ad8ffe0ad315e779
Vinayak Sachan <vinayakasachan@gmail.com>
2021-12-20 21:19:48 +0530

    md5 no salt

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ejs=require('ejs')
+const md5 = require('md5');
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,17 +11,13 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
-mongoose.connect('mongodb://localhost:27017/secretsDB', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/hashDB', { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
-const secret = process.env.secret;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 const user = mongoose.model('user', userSchema);
-
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -39,7 +36,7 @@ app.post('/register', function (req, res) {
 
     const newUser = new user({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save(function (err) {
@@ -54,7 +51,7 @@ app.post('/register', function (req, res) {
 
 app.post('/login', function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     user.findOne({ email: username }, function (err, foundUser) {
         if (err) {
@@ -62,7 +59,6 @@ app.post('/login', function (req, res) {
         }
         else {
             if (foundUser) {
-                console.log(foundUser)
                 if (foundUser.password === password) {
                     res.render('secrets')
                 }
